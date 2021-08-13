b47ea21de9c7b553c33b4843c6f4389e6aeb3562
lalith kumar <ucs17337@rmd.ac.in>
2021-08-13 13:39:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+function App() { 
+  const [emoji,setEmoji]=useState("happy");
+  let toggle=(e)=>{
+    if(emoji==="happy")
+    {
+      setEmoji("sad");
+    }
+    else
+    {
+      setEmoji("happy");
+    }
+  }
+
+  if(emoji==="happy")
+  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 style={{color:"green",cursor:"pointer"}} onClick={toggle}>:)</h1>
     </div>
   );
+  }
+  else
+  {
+    return (
+      <div className="App">
+        <h1 style={{color:"red",cursor:"pointer"}} onClick={toggle}>:(</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
